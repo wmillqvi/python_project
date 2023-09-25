f1192a69e2c3e46f2c0dd86378be1639d7fd2ebc
wmillqvi <william.millqvist@volvocars.com>
2023-09-25 14:32:47 +0200

    Added pipfile and modified routes

--- python_project/routes.py
+++ python_project/routes.py
@@ -1,11 +1,19 @@
-# routes 
-
-from flask import Flask
+from flask import Flask, request
 
 my_app = Flask(__name__)
 
+
 @my_app.route("/", methods=["GET"])
 def hello():
-    return "Hello World!"
+    return "Hello world!"
+
+
+@my_app.route("/postman", methods=["POST"])
+def mirror_data():
+    print("here")
+    data = request.get_json()
+    print(data)
+    return data
+
 
 my_app.run()
